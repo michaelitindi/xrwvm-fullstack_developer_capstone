d042fbd87c87f024b31d2b0633b3b1dd7a3cdb68
michaelitindi <michaelitindi@gmail.com>
2024-08-27 12:07:21 -0400

    sign up

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -39,12 +39,41 @@ def login_user(request):
     return JsonResponse(data)
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
+def logout_request(request):
+    logout(request)
+    data = {"userName":""}
+    return JsonResponse(data)
+
+
 
 # Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
+@csrf_exempt
+def registration(request):
+    context = {}
+
+    data = json.loads(request.body)
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+    username_exist = False
+    email_exist = False
+    try:
+        User.objects.get(username=username)
+        username_exist = True
+    except:
+        logger.debug("{} is new user".format(username))
+    
+    if not username_exist:
+        user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password, email=email)
+        login(request, user)
+        data = {"userName":username,"status":"Authenticated"}
+        return JsonResponse(data)
+    else:
+        data = {"userName":username,"error":"Already Registered"}
+        return JsonResponse(data)
+
 # ...
 
 # # Update the `get_dealerships` view to render the index page with
