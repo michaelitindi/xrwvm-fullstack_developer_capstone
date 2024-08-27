1b6c90cf6e5314c2637524fca9170161a6ae4785
michaelitindi <michaelitindi@gmail.com>
2024-08-27 10:09:02 -0400

    fixed login

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,7 +2,6 @@
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from django.views.generic import TemplateView
 from . import views
 
 app_name = 'djangoapp'
@@ -11,8 +10,7 @@ urlpatterns = [
 
     # path for login
      path(route='login', view=views.login_user, name='login'),
-     path('login/', TemplateView.as_view(template_name="index.html")),
-
+     
     # path for dealer reviews view
 
     # path for add a review view
